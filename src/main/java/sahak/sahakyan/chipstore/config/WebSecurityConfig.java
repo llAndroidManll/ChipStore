33e51f33a188f325aff2f099f766b1b19cb88a19
llAndroidManll <153990813+llAndroidManll@users.noreply.github.com>
2024-03-28 00:34:06 +0400

    Creating WebSecurityConfig class

--- src/main/java/sahak/sahakyan/chipstore/config/WebSecurityConfig.java
+++ src/main/java/sahak/sahakyan/chipstore/config/WebSecurityConfig.java
@@ -18,9 +18,9 @@ import javax.sql.DataSource;
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    /*private final UserDetailsService userDetailsService;
 
-    @Override
+    private final DataSource dataSource;
+    /*@Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
@@ -38,15 +38,41 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .logoutUrl("/logout") // Specify the logout URL
                 .logoutSuccessUrl("/login")
                 .permitAll();
+    }*/
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/registration","/verification","/verificationCheck","/static/**", "/").permitAll()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/user/**").hasAnyRole("ADMIN", "USER")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login") // Specify the login page URL
+                .failureUrl("/login-error")
+                .loginProcessingUrl("/authenticate")
+                .defaultSuccessUrl("/user/home")
+                .permitAll()
+                .and()
+                .logout()
+                .logoutUrl("/logout") // Specify the logout URL
+                .logoutSuccessUrl("/login")
+                .permitAll();
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("SELECT email, password, enabled FROM users WHERE email=?")
+                .authoritiesByUsernameQuery("SELECT email, role FROM users WHERE email=?")
+                .passwordEncoder(passwordEncoder());
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-    }*/
+    }
 }
