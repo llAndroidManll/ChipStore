8978e36da67023c3765330bf8007ab1bf7935bf1
llAndroidManll <153990813+llAndroidManll@users.noreply.github.com>
2024-03-28 00:31:28 +0400

    Creating HomeController

---
+++ src/main/java/sahak/sahakyan/chipstore/controller/HomeController.java
@@ -0,0 +1,12 @@
+package sahak.sahakyan.chipstore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
